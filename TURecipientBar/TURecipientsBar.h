d56cd701b4a336414618681858a4c133802c4a64
David Beck <david@davidbeck.co>
2013-10-23 12:07:34 -0700

    Added documentation for TURecipientsBar
    Added comment documentation using the AppleDoc spec.

--- TURecipientBar/TURecipientsBar.h
+++ TURecipientBar/TURecipientsBar.h
@@ -10,43 +10,203 @@
 
 #import "TURecipient.h"
 
-
 @protocol TURecipientsBarDelegate;
+
+
+/** The primary view for a recipient bar.
+ 
+ This view can be used independantly, but works best with TURecipientsDisplayController.
+ */
 
 
 @interface TURecipientsBar : UIScrollView <UITextFieldDelegate>
 
-@property (nonatomic, weak) IBOutlet NSLayoutConstraint *heightConstraint;
+/**---------------------------------------------------------------------------------------
+ * @name Text Content
+ *  ---------------------------------------------------------------------------------------
+ */
 
+/** The current or starting search text.
+ 
+ The default value is nil.
+ */
 @property (nonatomic, copy) NSString *text;
+
+/** The text for the label in front of both the search field and tokens.
+ 
+ The default value is "To: ".
+ 
+ You can set this to nil to disable the label entirely.
+ */
 @property (nonatomic, copy) NSString *label;
+
+/** The string that is displayed when there is no other text in the search field.
+ 
+ The default value is nil.
+ */
 @property (nonatomic, copy) NSString *placeholder;
-@property (nonatomic, readonly) NSArray *recipients;
-- (void)addRecipient:(TURecipient *)recipient;
-- (void)removeRecipient:(TURecipient *)recipient;
+
+/** Whether the bar is searching or not.
+ 
+ When the user taps on the bar it enters a searching mode that shows the recipients as tokens.
+ */
 @property (nonatomic) BOOL searching;
+
+/** Animate changes to searching.
+ 
+ Sets searching either animated or not. Note that this calls setSearching: inside of an animation block if needed.
+ 
+ @param searching What to set searching to.
+ @param animated Whether the change should be animated or not.
+ */
 - (void)setSearching:(BOOL)searching animated:(BOOL)animated;
+
+
+/**---------------------------------------------------------------------------------------
+ * @name Accessing Views
+ *  ---------------------------------------------------------------------------------------
+ */
 
 @property (nonatomic) UITextAutocapitalizationType autocapitalizationType __attribute__((deprecated));
 @property (nonatomic) UITextAutocorrectionType autocorrectionType __attribute__((deprecated));
 @property (nonatomic) UIKeyboardType keyboardType __attribute__((deprecated));
 @property (nonatomic) UITextSpellCheckingType spellCheckingType __attribute__((deprecated));
+
+/** The text field used for searching.
+ 
+ This text field moves to the front of the list of recipients.
+ 
+ You can use this to customize the text input attributes for searching.
+ */
 @property (nonatomic, readonly) UITextField *textField;
+
+/** Whether the add button should appear.
+ 
+ The add button calls the delegates method -recipientsBarAddButtonClicked:. It is a UIButtonTypeContactAdd type button.
+ */
 @property (nonatomic) BOOL showsAddButton;
 
+/** The height constraint for the entire bar.
+ 
+ You can use this to set the height constraint in Interface Builder. This was almost necessary in iOS 6, however in iOS 7, you can set a placeholder constraint and let the bar create it's own height constraint.
+ 
+ You can also change the priority. It must be lower than whatever constraint you are using to limit the bars height.
+ */
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *heightConstraint;
+
+
+/**---------------------------------------------------------------------------------------
+ * @name Delegate
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/** The recipients bar’s delegate object.
+ 
+ The delegate should conform to the TURecipientsBarDelegate protocol. Set this property to further modify the behavior. The default value is nil.
+ */
 @property (nonatomic, weak) id<TURecipientsBarDelegate> recipientsBarDelegate;
 
+
+/**---------------------------------------------------------------------------------------
+ * @name Recipients
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/** An array of all the recipients objects.
+ 
+ If you want to add or remove recipients, use -addRecipient: and -removeRecipient:.
+ 
+ Items are of type TURecipient.
+ */
+@property (nonatomic, readonly) NSArray *recipients;
+
+/** Add a recipient.
+ 
+ This will both add the recipient to the array of recipients and will also update the view, whether it is in searching mode or not.
+ 
+@param recipient A recipient to add.
+ */
+- (void)addRecipient:(TURecipient *)recipient;
+
+/** Remove a recipient.
+ 
+ This will both remove the recipient from the array of recipients and will also update the view, whether it is in searching mode or not.
+ 
+ @param recipient A recipient to remove.
+ */
+- (void)removeRecipient:(TURecipient *)recipient;
+
+/** Select a recipient.
+ 
+ If the bar is in search mode, you can use this to select the token that represents it. If the bar is not searching, this has no effect.
+ 
+ @param recipient A recipient to select.
+ */
 - (void)selectRecipient:(TURecipient *)recipient;
 
-// UIAppearance
+
+/**---------------------------------------------------------------------------------------
+ * @name Customizing Appearance
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/** Sets the background image for the token that represents a recipient.
+ 
+ Note that by default, UIControlStateNormal, UIControlStateHighlighted and UIControlStateSelected are set and must be overriden (thus, setting a image for normal will not be applied when the token is highlighted or selected). The other states are not currently used.
+ 
+ @param backgroundImage The background image to use. This should be a resizable image.
+ @param state The state to use this background in.
+ */
 - (void)setRecipientBackgroundImage:(UIImage *)backgroundImage forState:(UIControlState)state UI_APPEARANCE_SELECTOR;
+
+/** The background image for the token that represents a recipient.
+ 
+ Defaults to iOS 6 style token images. UIControlStateHighlighted and UIControlStateSelected use the same default image.
+ 
+ @param state The state to use this background in.
+ @return The background image for the given state.
+ */
 - (UIImage *)recipientBackgroundImageForState:(UIControlState)state UI_APPEARANCE_SELECTOR;
+
+/** The edge insets for the token that represents a recipient.
+ 
+ If you customize the recipient background image, you may need to customize the positioning of the title.
+ */
 @property (nonatomic) UIEdgeInsets recipientContentEdgeInsets UI_APPEARANCE_SELECTOR;
+
+/** The text attributes for the token that represents a recipient.
+ 
+ When present, these attributes will override the default ones for tokens. Any attribute that is not set here will use the default.
+ 
+ Note that by default, UIControlStateNormal, UIControlStateHighlighted and UIControlStateSelected are set and must be overriden (thus, setting a image for normal will not be applied when the token is highlighted or selected). The other states are not currently used.
+ 
+ @param state The state to use the attributes in.
+ */
 - (void)setRecipientTitleTextAttributes:(NSDictionary *)attributes forState:(UIControlState)state UI_APPEARANCE_SELECTOR;
+
+/** The text attributes for the token that represents a recipient.
+ 
+ When present, these attributes will override the default ones for tokens. Any attribute that is not set here will use the default.
+ 
+ Note that by default, UIControlStateNormal, UIControlStateHighlighted and UIControlStateSelected are set and must be overriden (thus, setting a image for normal will not be applied when the token is highlighted or selected). The other states are not currently used.
+ 
+ @param state The state to use this background in.
+ @return The text attributes override for the given state.
+ */
 - (NSDictionary *)recipientTitleTextAttributesForState:(UIControlState)state UI_APPEARANCE_SELECTOR;
 
+/** The text attributes applied to the label.
+ */
 @property (nonatomic, copy) NSDictionary *labelTextAttributes UI_APPEARANCE_SELECTOR;
+
+/** The text attributes applied to the summary.
+ */
 @property (nonatomic, copy) NSDictionary *summaryTextAttributes UI_APPEARANCE_SELECTOR;
+
+/** The text attributes applied to the placeholder.
+ 
+ Note that at this time, changing summaryTextAttributes or recipientTitleTextAttributesForState: will not change the placeholder attributes.
+ */
 @property (nonatomic, copy) NSDictionary *placeholderTextAttributes UI_APPEARANCE_SELECTOR;
 
 @end
@@ -56,17 +216,108 @@
 
 @optional
 
-- (BOOL)recipientsBarShouldBeginEditing:(TURecipientsBar *)recipientsBar;                      // return NO to not become first responder
-- (void)recipientsBarTextDidBeginEditing:(TURecipientsBar *)recipientsBar;                     // called when text starts editing
-- (BOOL)recipientsBarShouldEndEditing:(TURecipientsBar *)recipientsBar;                        // return NO to not resign first responder
-- (void)recipientsBarTextDidEndEditing:(TURecipientsBar *)recipientsBar;                       // called when text ends editing
-- (void)recipientsBar:(TURecipientsBar *)recipientsBar textDidChange:(NSString *)searchText;   // called when text changes (including clear)
-- (BOOL)recipientsBar:(TURecipientsBar *)recipientsBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text; // called before text changes
+/**---------------------------------------------------------------------------------------
+ * @name Editing Text
+ *  ---------------------------------------------------------------------------------------
+ */
 
+/** Asks the delegate if editing should begin in the specified search bar.
+ 
+ Return `NO` to not become first responder.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @return `YES` if an editing session should be initiated, otherwise, `NO`.
+ */
+- (BOOL)recipientsBarShouldBeginEditing:(TURecipientsBar *)recipientsBar;
+
+/** Tells the delegate when the user begins editing the search text.
+ 
+ Called when text starts editing.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ */
+- (void)recipientsBarTextDidBeginEditing:(TURecipientsBar *)recipientsBar;
+
+/** Asks the delegate if editing should stop in the specified search bar.
+ 
+ Return `NO` to not resign first responder.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @return `YES` if editing should stop, otherwise, `NO`.
+ */
+- (BOOL)recipientsBarShouldEndEditing:(TURecipientsBar *)recipientsBar;
+
+/** Tells the delegate that the user finished editing the search text.
+ 
+ Called when text ends editing.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ */
+- (void)recipientsBarTextDidEndEditing:(TURecipientsBar *)recipientsBar;
+
+/** Tells the delegate that the user changed the search text.
+ 
+ Called when text changes (including clear).
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @param searchText The current text in the search text field.
+ */
+- (void)recipientsBar:(TURecipientsBar *)recipientsBar textDidChange:(NSString *)searchText;
+
+/** Asks the delegate if editing should stop in the specified recipient bar.
+ 
+ Called before text changes.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @param range The range of the text to be changed.
+ @param text The text to replace existing text in range.
+ @return `YES` if text in range should be replaced by text, otherwise, `NO`.
+ */
+- (BOOL)recipientsBar:(TURecipientsBar *)recipientsBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
+
+/**---------------------------------------------------------------------------------------
+ * @name Selection
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/** Asks the delegate if recipients should be selected.
+ 
+ Return no to disable selection.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @param recipient The recipient being selected.
+ @return `YES` if the recipient should be selected, otherwise, `NO`.
+ */
 - (BOOL)recipientsBar:(TURecipientsBar *)recipientsBar shouldSelectRecipient:(TURecipient *)recipient;
+
+/** Tells the delegate that recipient selection has changed.
+ 
+ Called when the user selects a recipient, either with the back delete key, or by tapping them.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ @param recipient The recipient that was selected.
+ */
 - (void)recipientsBar:(TURecipientsBar *)recipientsBar didSelectRecipient:(TURecipient *)recipient;
 
-- (void)recipientsBarReturnButtonClicked:(TURecipientsBar *)recipientsBar;                     // called when keyboard return button pressed
-- (void)recipientsBarAddButtonClicked:(TURecipientsBar *)recipientsBar;                        // called when add button pressed
+/**---------------------------------------------------------------------------------------
+ * @name Clicking Buttons
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/**  Tells the delegate when the return key is pressed.
+ 
+ Called when the keyboard return button is pressed.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ */
+- (void)recipientsBarReturnButtonClicked:(TURecipientsBar *)recipientsBar;
+
+/**  Tells the delegate when the add button is pressed.
+ 
+ Called when the add button is pressed. Use this to show recipient selection UI.
+ 
+ @param recipientsBar The recipient bar that is being edited.
+ */
+- (void)recipientsBarAddButtonClicked:(TURecipientsBar *)recipientsBar;
 
 @end
